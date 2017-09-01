ae313214e97521c921c0c125e110b831b7247c6a
DeedleFake <deedlefake@users.noreply.github.com>
2017-09-01 15:44:56 -0400

    groestl256: Rearrange and fix some things.

--- groestl256/groestl256.go
+++ groestl256/groestl256.go
@@ -1,45 +1,54 @@
 package groestl256
 
+import (
+	"hash"
+)
+
 const (
 	Size      = 32
 	BlockSize = 4
 )
 
-type hash struct {
-	nfactor int
+type impl struct {
+	buf   [64]byte
+	ptr   uintptr
+	state [8]uint64
+	count uint64
 
 	data []byte
-	sum  []byte
 }
 
 func New() hash.Hash {
-	return &hash{
-		nfactor: nfactor,
-	}
+	h := &impl{}
+	h.Reset()
+	return h
 }
 
-func (h *hash) Write(data []byte) (int, error) {
-	h.data = append(data)
-	h.sum = Sum(h.data, h.nfactor)[:]
+func (h *impl) Write(data []byte) (int, error) {
+	h.data = append(h.data, data...)
 	return len(data), nil
 }
 
-func (h *hash) Sum(data []byte) []byte {
-	return append(data, h.sum)
+func (h *impl) Sum(data []byte) []byte {
+	panic("Not implemented.")
 }
 
-func (h *hash) Reset() {
-	*h = hash{}
+func (h *impl) Reset() {
+	*h = impl{}
+	h.state[7] = Size * 8
 }
 
-func (h *hash) Size() {
+func (h *impl) Size() int {
 	return Size
 }
 
-func (h *hash) BlockSize() {
+func (h *impl) BlockSize() int {
 	return BlockSize
 }
 
-func Sum(data []byte) [Size]byte {
-	panic("Not implemented.")
+func Sum(data []byte) (out [Size]byte) {
+	h := New()
+	h.Write(data)
+	copy(out[:], h.Sum(nil))
+	return
 }
