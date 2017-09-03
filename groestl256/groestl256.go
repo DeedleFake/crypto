1736d7c742e9655b3acc0a20df60e048064b9d32
DeedleFake <deedlefake@users.noreply.github.com>
2017-09-03 18:31:21 -0400

    groestl256: Completely rework. Use same structure as original C code.

--- groestl256/groestl256.go
+++ groestl256/groestl256.go
@@ -6,63 +6,49 @@ import (
 )
 
 const (
-	Size      = 64
+	Size      = 32
 	BlockSize = 64
 )
 
-type impl struct {
+type context struct {
 	buf    [64]byte
 	offset int
 	state  [8]uint64
-
-	data []byte
+	count  uint64
 }
 
-func New() hash.Hash {
-	h := &impl{}
-	h.Reset()
-	return h
+func (ctx *context) init(size uint64) {
+	// Most of these are set to zero values, but they'll need to be
+	// reinitialized later, so...
+
+	ctx.offset = 0
+	ctx.state = [8]uint64{}
+	ctx.state[7] = size
+	ctx.count = 0
 }
 
-func (h *impl) stateBytes() (out []byte) {
-	out = make([]byte, Size)
-	for i := range h.state {
-		binary.BigEndian.PutUint64(out[i*8:], h.state[i])
+func (ctx *context) core(data []byte) {
+	if len(data) < len(ctx.buf)-ctx.offset {
+		copy(ctx.buf[ctx.offset:], data)
+		ctx.offset += len(data)
+		return
 	}
-	return
-}
-
-func (h *impl) Write(data []byte) (int, error) {
-	h.data = append(h.data, data...)
-	return len(data), nil
-}
-
-func (h *impl) pad(clen int) {
-	panic("Not implemented.")
-}
-
-func (h *impl) Sum(prev []byte) []byte {
-	data := h.data
 
 	for len(data) > 0 {
-		clen := len(h.buf) - h.offset
+		clen := len(ctx.buf) - ctx.offset
 		if clen > len(data) {
 			clen = len(data)
 		}
 
-		copy(h.buf[h.offset:], data[:clen])
-		if len(data) < BlockSize {
-			h.pad(clen)
-		}
-
-		h.offset += clen
+		copy(ctx.buf[ctx.offset:], data[:clen])
+		ctx.offset += clen
 		data = data[clen:]
 
-		if h.offset == len(h.buf) {
+		if ctx.offset == len(ctx.buf) {
 			var g, m [8]uint64
-			for u := range h.state {
-				m[u] = binary.BigEndian.Uint64(h.buf[u<<3:])
-				g[u] = m[u] ^ h.state[u]
+			for u := range ctx.state {
+				m[u] = binary.BigEndian.Uint64(ctx.buf[u<<3:])
+				g[u] = m[u] ^ ctx.state[u]
 			}
 
 			for r := 0; r < 10; r += 2 {
@@ -75,20 +61,76 @@ func (h *impl) Sum(prev []byte) []byte {
 				roundSmallQ(m[:], uint64(r+1))
 			}
 
-			for u := range h.state {
-				h.state[u] ^= g[u] ^ m[u]
-			}
+			for u := range ctx.state {
+				ctx.state[u] ^= g[u] ^ m[u]
 
-			h.offset = 0
+				ctx.count++
+				ctx.offset = 0
+			}
 		}
 	}
+}
 
-	return append(prev, h.stateBytes()...)
+func (ctx *context) close(dst []byte, ub, n uint64) {
+	var pad [72]byte
+
+	z := uint64(0x80) >> n
+	pad[0] = uint8((ub & -z) | z)
+
+	padLen := 128 - ctx.offset
+	count := ctx.count + 2
+	if ctx.offset < 56 {
+		padLen = 64 - ctx.offset
+		count = ctx.count + 1
+	}
+	binary.BigEndian.PutUint64(pad[padLen-8:], count)
+
+	ctx.core(pad[:])
+
+	x := ctx.state
+	for r := 0; r < 10; r += 2 {
+		roundSmallP(x[:], uint64(r))
+		roundSmallP(x[:], uint64(r+1))
+	}
+
+	for u := range x {
+		ctx.state[u] ^= x[u]
+	}
+	for u := 0; u < 4; u++ {
+		binary.BigEndian.PutUint64(pad[u<<3:], ctx.state[u+4])
+	}
+
+	copy(dst, pad[32-len(dst):])
+
+	ctx.init(uint64(len(dst)) << 3)
+}
+
+type impl struct {
+	ctx  context
+	data []byte
+}
+
+func New() hash.Hash {
+	h := &impl{}
+	h.Reset()
+	return h
+}
+
+func (h *impl) Write(data []byte) (int, error) {
+	h.data = append(h.data, data...)
+	return len(data), nil
+}
+
+func (h *impl) Sum(prev []byte) []byte {
+	out := append(prev, make([]byte, Size)...)
+	h.ctx.core(h.data)
+	h.ctx.close(out[len(prev):], 0, 0)
+	return out
 }
 
 func (h *impl) Reset() {
-	*h = impl{}
-	h.state[7] = Size * 8
+	h.ctx.init(Size)
+	h.data = h.data[:0]
 }
 
 func (h *impl) Size() int {
